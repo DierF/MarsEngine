a38cf25ddec09760f31dde2082cf05accc91130f
DierF <fengdier2@126.com>
2023-05-21 18:01:36 -0400

    add sample assets in sandbox.

---
+++ Sandbox/assets/shaders/Texture.glsl
@@ -0,0 +1,32 @@
+//basic texture shader
+
+#type vertex
+#version 330 core
+			
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec2 a_textCoord;
+
+uniform mat4 u_viewProjection;
+uniform mat4 u_transform;
+
+out vec2 v_textCoord;
+
+void main()
+{
+	v_textCoord = a_textCoord;
+	gl_Position = u_viewProjection * u_transform * vec4(a_position, 1.0);
+}
+
+#type fragment
+#version 330 core
+			
+layout(location = 0) out vec4 color;
+			
+in vec2 v_textCoord;
+
+uniform sampler2D u_texture;
+
+void main()
+{
+	color = texture(u_texture, v_textCoord);
+}
